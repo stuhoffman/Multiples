ab385f39e05d9a9317f2a570c8ffcce9b028bb64
Stuart Hoffman <stu@hoffmanswebsite.com>
2016-02-18 13:10:12 -0800

    First commit for Multiples2 going to add to Multiples in GH

--- Multiples2/ViewController.swift
+++ Multiples2/ViewController.swift
@@ -8,8 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
+    //props
+    var startingValue: Int? = 0
+    var currentSum: Int = 0
 
+    //Outlets
+    @IBOutlet var addText: UITextField!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var logoImage: UIImageView!
+    
+    @IBOutlet var displayText: UITextField!
+    @IBOutlet var addButton: UIButton!
+    
+    //Actions
+    @IBAction func addButtonPressed(sender: UIButton) {
+        if (addIsValid())
+        {
+            currentSum = currentSum + startingValue!
+        }
+        
+        displayText.text = "\(currentSum)"
+        
+    }
+    @IBAction func playButtonPressed(sender: UIButton) {
+        logoImage.hidden = true
+        addText.hidden = true
+        playButton.hidden = true
+        
+        displayText.hidden = false
+        addButton.hidden = false
+        
+        startingValue = Int(addText.text!)
+
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        addText.resignFirstResponder()
+        return true
+    }
+    
+    func addIsValid() -> Bool {
+        if (addText.text != nil && addText.text != "")
+        {
+            NSLog("The add is valid")
+            return true
+        }
+        NSLog("Not Valid")
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
